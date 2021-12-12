8086a56b75c152ffb08d25c884580dfa26a3f4ae
Abhishek Yadav <bachiyadav1270@gmail.com>
2021-12-12 23:44:28 +0530

    create view for home

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port =8000;
 //use express routes
 app.use('/' , require('./routes'));
 
+//Set up the view engine
+app.set('view engine','ejs');
+app.set('views' , './views');
+
 
 app.listen(port ,  function(err){
     if(err){
